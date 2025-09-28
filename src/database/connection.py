1efe78d43a06ac0953475382241bd51fc3afe945
ebochkov <ebochkov@itkey.com>
2025-09-28 11:24:00 +0300

    `wip`

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import Base
-from config.settings import DB_CONFIG
+from src.config.settings import DB_CONFIG
 
 
 class DatabaseManager:
