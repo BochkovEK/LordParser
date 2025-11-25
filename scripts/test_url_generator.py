af5a82298057c8d969c5ca426f4c47d7c474cd3f
ebochkov <ebochkov@itkey.com>
2025-11-25 15:21:37 +0300

    `wip`

--- scripts/test_url_generator.py
+++ scripts/test_url_generator.py
@@ -5,8 +5,9 @@ Test URL Generator - emulates usage from future scheduler.py
 import os
 import sys
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
-from src.utils.url_generator import URLGenerator
+
+# sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+from src.parser.url_generator import URLGenerator
 
 
 async def test_url_generator():
